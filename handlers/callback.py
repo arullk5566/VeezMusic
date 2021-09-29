5eaba9afddffca00ed3a18705df11284bcb533fb
arullk5566 <85780324+arullk5566@users.noreply.github.com>
2021-09-29 13:15:59 +0700

    Update callback.py

--- handlers/callback.py
+++ handlers/callback.py
@@ -39,10 +39,10 @@ async def cbstart(_, query: CallbackQuery):
                         "📣 Official Channel", url=f"https://t.me/{UPDATES_CHANNEL}")
                 ],[
                     InlineKeyboardButton(
-                        "🌐 Wiki's Page", url="https://github.com/levina-lab/veezmusic/wiki/Veez-Music-Wiki's")
+                        "🌐 Wiki's Page", url="https://github.com/arullk5566/veezmusic/wiki/klimanam-Music-Wiki's")
                 ],[
                     InlineKeyboardButton(
-                        "🧪 Source Code 🧪", url="https://github.com/levina-lab/VeezMusic"
+                        "🧪 Source Code 🧪", url="https://github.com/arullk5566/VeezMusic"
                     )
                 ]
             ]
